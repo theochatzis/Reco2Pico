bb13add80b7a287986d7f5bc9d77330802bd78b1
Theo Chatzistavrou <teophysix@gmail.com>
2026-08-14 16:05:38 +0200

    rdf exe

--- analysis/RhoStripStudy/makePlots_RDF_exe.sh
+++ analysis/RhoStripStudy/makePlots_RDF_exe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python3 make_pf_rho_strip_plots_rdf.py /eos/user/t/tchatzis/reco2pico/CMSSW_15_0_4/src/Reco2Pico/PicoProducer/python/workflows/test_pico_withPFHC.root \
-  -o /eos/user/t/tchatzis/php-plots/PFNoiseStudy/qcd/WithPFHC/ \
+python3 make_pf_rho_strip_plots_rdf.py /eos/user/t/tchatzis/reco2pico/CMSSW_15_0_4/src/Reco2Pico/PicoProducer/python/workflows/test_pico.root \
+  -o /eos/user/t/tchatzis/php-plots/PFNoiseStudy/NeutrinoGunHF \
   --root-output pf_rho_strip_plots_rdf.root \
   --prefix PFRhoStrip \
   --nvtx-branch PV_npvsGood \
